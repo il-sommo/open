1532ac57e86ff7092591cf66c7259ca1d94d8f30
Fabrizio Vacca <fabrizio.vacca@telsy.lan>
2013-02-06 08:47:49 +0100

    Passetti avanti

--- ppc_cfg.sh
+++ ppc_cfg.sh
@@ -11,6 +11,6 @@ ARCH=ppc CROSS_COMPILE=ppc_4xx- \
 LDFLAGS="-static" POLARSSL_CFLAGS="-I${here}/../polar/include/" \
 POLARSSL_LIBS="-L${here}/../polar/library/ -lpolarssl" \
 ./configure --host=powerpc-linux --disable-lzo --with-crypto-library=polarssl \
---disable-plugin-auth-pam  --disable-plugin-down-root && \
+--disable-plugins --disable-plugin-auth-pam  --disable-plugin-down-root && \
 ARCH=ppc CROSS_COMPILE=ppc_4xx- CC=ppc_4xx-gcc make -j4
 
--- x86_cfg.sh
+++ x86_cfg.sh
@@ -11,5 +11,5 @@ cd ${here}
 LDFLAGS="-static" POLARSSL_CFLAGS="-I${here}/../polar/include/" \
 POLARSSL_LIBS="-L${here}/../polar/library/ -lpolarssl" \
 ./configure --disable-lzo --with-crypto-library=polarssl \
---disable-plugin-auth-pam  --disable-plugin-down-root && \
+--disable-plugins --disable-plugin-auth-pam  --disable-plugin-down-root && \
 make -j4
